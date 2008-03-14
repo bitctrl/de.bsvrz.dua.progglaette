4c939bac3938d3306330f9b6de1379ee206263a7
tfelder <info@bitctrl.de>
2008-03-14 15:31:07 +0000

    bash
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.progglaette@7458 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ extra/skripte-bash/progglaette.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ../../../skripte-bash/einstellungen.sh
+
+################################################################################
+# SWE-Spezifische Parameter	(überprüfen und anpassen)                          #
+################################################################################
+
+kb="KonfigurationsBereichsPid="
+
+################################################################################
+# Folgende Parameter müssen überprüft und evtl. angepasst werden               #
+################################################################################
+
+# Parameter für den Java-Interpreter, als Standard werden die Einstellungen aus # einstellungen.sh verwendet.
+#jvmArgs="-Dfile.encoding=ISO-8859-1"
+
+# Parameter für den Datenverteiler, als Standard werden die Einstellungen aus # einstellungen.sh verwendet.
+#dav1="-datenverteiler=localhost:8083 -benutzer=Tester -authentifizierung=passwd -debugFilePath=.."
+
+################################################################################
+# Ab hier muss nichts mehr angepasst werden                                    #
+################################################################################
+
+# Applikation starten
+java $jvmArgs -jar ../de.bsvrz.dua.proglaette-runtime.jar \
+	$dav1 \
+	$kb \
+	-debugLevelFileText=all \
+	-debugLevelStdErrText=:error \
+	-debugSetLoggerAndLevel=:none \
+	-debugSetLoggerAndLevel=de.bsvrz.iav:config \
+	&
