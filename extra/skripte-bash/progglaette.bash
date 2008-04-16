06808f9843ee5401a03cc7220337cc94ef2d0d5d
tfelder <info@bitctrl.de>
2008-04-16 17:22:04 +0000

    checkstyle
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.progglaette@8366 40c0be50-5d29-0410-901d-fb59edb5cafc

--- extra/skripte-bash/progglaette.bash
+++ extra/skripte-bash/progglaette.bash
@@ -10,7 +10,7 @@ source ../../../skripte-bash/einstellungen.sh
 # SWE-Spezifische Parameter	(überprüfen und anpassen)                          #
 ################################################################################
 
-kb="-KonfigurationsBereichsPid=kb.UFD_Konfig_A8,kb.UFD_Konfig_B27"
+kb="kb.UFD_Konfig_A8,kb.UFD_Konfig_B27"
 
 ################################################################################
 # Folgende Parameter müssen überprüft und evtl. angepasst werden               #
@@ -22,6 +22,12 @@ kb="-KonfigurationsBereichsPid=kb.UFD_Konfig_A8,kb.UFD_Konfig_B27"
 # Parameter für den Datenverteiler, als Standard werden die Einstellungen aus # einstellungen.sh verwendet.
 #dav1="-datenverteiler=localhost:8083 -benutzer=Tester -authentifizierung=passwd -debugFilePath=.."
 
+jconPort="10414"
+
+if [ "$testlauf" ]; then
+	jvmArgs=$jvmArgs" -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port="$jconPort" -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+fi
+
 ################################################################################
 # Ab hier muss nichts mehr angepasst werden                                    #
 ################################################################################
@@ -29,7 +35,7 @@ kb="-KonfigurationsBereichsPid=kb.UFD_Konfig_A8,kb.UFD_Konfig_B27"
 # Applikation starten
 java $jvmArgs -jar ../de.bsvrz.dua.progglaette-runtime.jar \
 	$dav1 \
-	$kb \
+	-KonfigurationsBereichsPid=$kb \
 	-debugLevelStdErrText=ERROR \
 	-debugLevelFileText=ALL \
 	-debugLevelFileXML=OFF \
